62dc725db01ea352516c4811a10c3fb89d3a0e6f
Akili Humphrey <akilihumphrey@gmail.com>
2019-02-20 06:03:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
 
+  get '/square/:number' do
+    @num = params[:number].to_i
+    "#{@num**2}"
+  end
 end
