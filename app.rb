455a357bd825883843e42b2aba0999e607d8a1f7
Akili Humphrey <akilihumphrey@gmail.com>
2019-02-20 06:27:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,8 @@ class App < Sinatra::Base
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    params[:word1+:word2+:word3+:word4+:word5+"."]
+    words = params[:word1+:word2+:word3+:word4+:word5+"."]
+    "#{words}"
   end
   
 end
