364835070501fa4e7f5089f29711c7131e22ad33
Akili Humphrey <akilihumphrey@gmail.com>
2019-02-20 06:26:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,8 +18,7 @@ class App < Sinatra::Base
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @num = params[:number].to_i
-    "#{@num**2}"
+    params[:word1+:word2+:word3+:word4+:word5+.]
   end
   
 end
