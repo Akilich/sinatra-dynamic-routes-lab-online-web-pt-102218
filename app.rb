f1dd947c70cb4b9432eef0ea75c6354c562b09c5
Akili Humphrey <akilihumphrey@gmail.com>
2019-02-20 06:10:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,6 +13,6 @@ class App < Sinatra::Base
   
   get '/say/:number/:phrase' do
     @num = params[:number].to_i
-    10.times { puts "show me the money" }
+    n.times { puts "show me the money" }
   end
 end
