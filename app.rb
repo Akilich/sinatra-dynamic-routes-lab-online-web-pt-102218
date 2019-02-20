219748590b35390e14e0d83a36946e5493eba8c2
Akili Humphrey <akilihumphrey@gmail.com>
2019-02-20 06:38:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,4 +21,9 @@ class App < Sinatra::Base
     "#{params[:word1] +" "+ params[:word2]+" "+params[:word3]+" "+params[:word4]+" "+params[:word5]+"."}"
   end
   
+  get '/square/:number' do
+    @num = params[:number].to_i
+    "#{@num**2}"
+  en
+  
 end
